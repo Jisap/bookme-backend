78f090eb2b7818616f03ba47162ca792d37e4e01
Jorge Matos <jisapjisap@gmail.com>
2026-09-22 16:46:27 +0200

    Availability schema

---
+++ models/Availability.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+
+const SlotSchema = new mongoose.Schema(
+  {
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+  },
+  { _id: false },
+);
+
+const availabilitySchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    dayOfWeek: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 6,
+    },
+    slot: {
+      type: [SlotSchema],
+      default: []
+    }
+  },
+  { timestamps: true }
+);
+
+availabilitySchema.index({ userId: 1, dayOfWeek: 1 }, { unique: true });
+
+const Availability = mongoose.model("Availability", availabilitySchema);
+
+export default Availability;

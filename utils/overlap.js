b89cd298e89a6c466724523fc44c7feccff42020
Jorge Matos <jisapjisap@gmail.com>
2026-09-22 17:08:31 +0200

    More utils

---
+++ utils/overlap.js
@@ -0,0 +1,11 @@
+import { timeToMinutes } from "./time";
+
+// timeOverlap: Chekea si hay un solapamiento en los horarios.
+// 
+// Devuelve true si los rangos de tiempo se solapan, false en caso contrario.
+export const timeOverlap = (firstStart, firstEnd, secondStart, secondEnd) => {
+  // Se mide el tiempo en minutos para comparar los horarios
+  // Se comprueba si el inicio del primer rango es menor que el final del segundo
+  // y si el final del primer rango es mayor que el inicio del segundo
+  return timeToMinutes(firstStart) < timeToMinutes(secondEnd) && timeToMinutes(firstEnd) > timeToMinutes(secondStart)
+}
